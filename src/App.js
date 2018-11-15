237075cae515c34caae2710714bb8bbc641f807c
Juan Huttemann <juanfhuttemann@gmail.com>
2018-11-15 13:37:12 -0300

    agrega componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header
+          titulo="Clima React"
+        />
       </div>
     );
   }
