e6e7432c506af8a174f75d91a04854b1e98ef4a5
Juan Huttemann <juanfhuttemann@gmail.com>
2018-11-21 12:32:26 -0300

    import Error and Clima components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
+import Error from './componentes/Error';
+import Clima from './componentes/Clima';
 
 class App extends Component {
 
+  state = {
+    error: '',
+    consulta: {},
+    resultado: {}
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.consulta !== this.state.consulta) {
+      this.consultarApi();
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      error: false
+    })
+  }
+
+  consultarApi = () => {
+    const {ciudad, pais} = this.state.consulta;
+    if(!ciudad || !pais) return null;
+
+    const appId= "895e264c64ed20cdad835f05afb27b08"
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+    
+    fetch(url)
+      .then( respuesta => {
+        return respuesta.json();
+      })
+      .then(datos=>{
+        this.setState({
+          resultado: datos
+        })
+      })
+      .catch(error=>{
+        console.log(error);
+      })
+  }
+
   datosConsulta = respuesta => {
     if (respuesta.ciudad === '' || respuesta.pais === '') {
-      console.log('errores');
+      this.setState({
+        error: true
+      })
     } else {
-      console.log('todo correcto');
+      this.setState({
+        consulta: respuesta,
+        error: false
+      })
     }
   }
 
   render() {
+
+    const error = this.state.error;
+
+    let resultado;
+
+    if (error) {
+      resultado = <Error mensaje="Ambos campos son obligatorios"/>
+    } else {
+      resultado = <Clima resultado = {this.state.resultado} />
+    }
+
     return (
       <div className="App">
         <Header
@@ -21,6 +78,7 @@ class App extends Component {
         <Formulario
           datosConsulta = {this.datosConsulta}
         />
+        { resultado }
       </div>
     );
   }
